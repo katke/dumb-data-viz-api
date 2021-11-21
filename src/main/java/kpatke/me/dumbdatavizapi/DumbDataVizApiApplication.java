ac2914e05af48f8bc27b45465c7caecb3ddf1826
Kat <kmmpatke@gmail.com>
2021-11-20 18:55:40 -0600

    initial commit

---
+++ src/main/java/kpatke/me/dumbdatavizapi/DumbDataVizApiApplication.java
@@ -0,0 +1,13 @@
+package kpatke.me.dumbdatavizapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumbDataVizApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumbDataVizApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kpatke/me/dumbdatavizapi/DumbDataVizApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kpatke.me.dumbdatavizapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DumbDataVizApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
